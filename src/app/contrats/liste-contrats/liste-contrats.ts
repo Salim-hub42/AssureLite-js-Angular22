8218460f3a14b6fb2bfaed9b7013e51438a23813
salim-hub42 <salim.khalfoun@yahoo.fr>
2026-08-10 15:00:15 +0200

    signal , computed sur function pur primeTotal

--- src/app/contrats/liste-contrats/liste-contrats.ts
+++ src/app/contrats/liste-contrats/liste-contrats.ts
@@ -1,14 +1,37 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { CONTRATS_MOCKS } from '../../data/mock-data';
 import { Contrat } from '../../models/contrat.model';
+import { Table } from 'primeng/table';
+import { primeTotal } from '../../models/contrat.utils';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-liste-contrats',
-  imports: [],
+  imports: [Table, ButtonModule],
   templateUrl: './liste-contrats.html',
   styleUrl: './liste-contrats.scss',
 })
 export class ListeContrats {
-
   contrats = signal<Contrat[]>(CONTRATS_MOCKS);
+  primeTotale = computed(() => primeTotal(this.contrats()));
+
+ ajouterContrat(){
+   this.contrats.update((listes) => {
+    const copie = [...listes];
+    copie.push({id: copie.length + 1, clientId: 1, type: 'auto', statut: 'actif', prime: 75, dateDebut: new Date()});
+    return copie
+   })
+ }
+
+
+
+
+
+
+
+
+
+
+
 }
+
