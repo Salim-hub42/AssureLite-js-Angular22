1a8e57e8e25d02ac73e95d57b14fba6f85d26c3c
salim-hub42 <salim.khalfoun@yahoo.fr>
2026-08-24 15:12:31 +0200

    start coding router component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+   {path: 'contrats' , 
+      loadComponent: () => import('./contrats/liste-contrats/liste-contrats').then((m) => m.ListeContrats)
+   },
+   {path: '' , redirectTo: 'contrats' , pathMatch:'full'},
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ListeContrats } from './contrats/liste-contrats/liste-contrats';
 
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ListeContrats],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
